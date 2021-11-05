0dea6068b166ab17db24a5094432ebe6ffbc1130
Robbie Brush <W0741993@myscc.ca>
2021-11-05 18:14:28 -0400

    Finished main screen layout

--- app/src/main/java/com/example/toofit/MainActivity.java
+++ app/src/main/java/com/example/toofit/MainActivity.java
@@ -29,19 +29,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.myToolbar);
-        binding.appBarMain.fabButt.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_main)
+                R.id.nav_main, R.id.nav_proteins)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
